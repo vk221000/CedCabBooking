a1d9065521a598a3b8adc6d2696190ea45ea7857
vivek kushwaha <65606054+vk221000@users.noreply.github.com>
2021-01-09 15:10:53 +0530

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -6,10 +6,10 @@ class dbconnect{
     public $dbname;
     public $conn;
     public function __construct(){
-        $this->servername='localhost';
-        $this->username='root';
-        $this->password='';
-        $this->dbname='cabbooking';
+        $this->servername='btqnyngtn1riwmgfbxhw-mysql.services.clever-cloud.com';
+        $this->username='uzrdkcha3pcetciw';
+        $this->password='r4azbEDvGVTQKSlqZIdd';
+        $this->dbname='btqnyngtn1riwmgfbxhw';
     }
     public function createConnection(){
         $this->conn=new mysqli($this->servername, $this->username, $this->password, $this->dbname);
